96d66a2dc667b4f22e5b1575fbecd072d159966c
SJ91Codes <99019986+SJ91Codes@users.noreply.github.com>
2022-11-01 01:19:25 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
+const Player = (piece, player) =>{
+    piece = piece;
+    player = player;
+    positions = []
+
+    return{piece, player, positions}
+
+}
+
 //gameboard module
 const gameboard = (() => {
+    // array containing player positions
     let boardArray = []
 
-    const move = (player) => {
-        boardArray.push(player)
-        console.log("hello")
-    }
-
+    // function to create the board
     const create = () =>{
         const hBoard = document.querySelector(".game-board")
+        // create each position, assign position value as data attribute and add listener
         for(x=0; x<9; x++){
             const gPiece = document.createElement("div")
             hBoard.appendChild(gPiece)
@@ -18,40 +25,83 @@ const gameboard = (() => {
         }
     }
 
-    const place = function(e){
-        let toPlace = e.path[0].attributes[1].value
-        boardArray[toPlace] = displayControl.player
-        displayControl.render
+    const compare = (player) =>{
+        const winningCombos = [[0,1,2], [0,3,6], [0,4,8], [1,4,7], [2,5,8], [2,4,6], [3,4,5], [6,7,8]]
+        //cycle through each winning combo
+        winningCombos.forEach(val=>{
+            let check = 0
+            //for each position in winning combo, check if its in the players positions
+            val.forEach(pos =>{
+                //if winning combo place in player positions icrement the check variable
+                if(player.positions.includes(pos)){
+                check++
+                }
+            })
+            // if 3 winning combos in player positions then player has won
+            if(check ===3){
+                console.log("winner")
+            }
+
+        })
+    
+            
+    
+
+
+
     }
-    return{create, move, boardArray}
+    // function to place player piece on relevant board piece
+    const place = function(e){
+        //get the data pos attribute from the event when piece clicked
+        let toPlace = e.path[0].attributes[1].value
+        // place the player piece in the board array in index that relates to board position
+        boardArray[toPlace] = displayControl.getPlayer()
+        //add selected position to players array, converted to int
+        player.positions.push(Number(toPlace))
+        //place all positions on board and change current player
+        displayControl.render()
+        compare(player)
+        displayControl.changePlayer()
+        
+    }
+    return{create, boardArray}
     
     
 })();
-
+//module to control display of board and pieces
 const displayControl = (()=>{
-    const player = "x"
-
+    //function to change current player
     const changePlayer = () =>{
-        if(player === "x"){
-            player = "o"
+        if(player === player1){
+            player = player2
         }else{
-            player = "x"
+            player = player1
         }
     }
+    //return current player
     const getPlayer = () =>{
-        return player
+        return player.piece
     }
-
+    //function to place positions on board
     const render = () => {
+        //cycle through array and place relevant pieces in relevant board positions
         gameboard.boardArray.forEach((val, index)=>{
             toPlace = document.querySelector(`[data-pos = "${index}"] `)
             toPlace.innerText = val
+            
 
         }
         )
     }
-    return{getPlayer, render}
+    return{getPlayer, render, changePlayer}
 
 })();
 gameboard.create()
+const player1 = Player("x", 1)
+const player2 = Player("o", 2)
+
+
+
+let player = player1
+
 
