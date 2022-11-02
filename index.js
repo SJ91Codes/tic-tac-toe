cfeb49a812491b89c4c164428f42d90fec2f788c
SJ91Codes <99019986+SJ91Codes@users.noreply.github.com>
2022-11-02 03:40:01 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ const Player = (piece, player) =>{
 const gameboard = (() => {
     // array containing player positions
     let boardArray = []
+    let toPlay
+
+
 
     // function to create the board
     const create = () =>{
@@ -21,7 +24,7 @@ const gameboard = (() => {
             hBoard.appendChild(gPiece)
             gPiece.classList.add("piece")
             gPiece.setAttribute("data-pos", x)
-            gPiece.addEventListener("click", place)
+            gPiece.addEventListener("click", displayControl.place)
         }
     }
 
@@ -39,32 +42,25 @@ const gameboard = (() => {
             })
             // if 3 winning combos in player positions then player has won
             if(check ===3){
-                console.log("winner")
+                winner()
             }
 
         })
     
             
-    
+    }
 
-
+    const winner= ()=>{
+        const hBoard = document.querySelector(".game-board")
+        hBoard.classList.add("hidden")
+        const result = document.createElement("div")
+        result.classList.add("result")
+        result.innerText=`${player.piece} wins`
+        document.body.appendChild(result)
 
     }
-    // function to place player piece on relevant board piece
-    const place = function(e){
-        //get the data pos attribute from the event when piece clicked
-        let toPlace = e.path[0].attributes[1].value
-        // place the player piece in the board array in index that relates to board position
-        boardArray[toPlace] = displayControl.getPlayer()
-        //add selected position to players array, converted to int
-        player.positions.push(Number(toPlace))
-        //place all positions on board and change current player
-        displayControl.render()
-        compare(player)
-        displayControl.changePlayer()
-        
-    }
-    return{create, boardArray}
+   
+    return{create, boardArray, compare, toPlay}
     
     
 })();
@@ -86,22 +82,65 @@ const displayControl = (()=>{
     const render = () => {
         //cycle through array and place relevant pieces in relevant board positions
         gameboard.boardArray.forEach((val, index)=>{
-            toPlace = document.querySelector(`[data-pos = "${index}"] `)
+            let toPlace = document.querySelector(`[data-pos = "${index}"] `)
             toPlace.innerText = val
             
 
         }
         )
     }
-    return{getPlayer, render, changePlayer}
+
+    // function to place player piece on relevant board piece
+    const place = (e)=>{
+        //get the data pos attribute from the event when piece clicked
+        let toPlace = e.path[0].attributes[1].value
+        // place the player piece in the board array in index that relates to board position
+        gameboard.boardArray[toPlace] = getPlayer()
+        //add selected position to players array, converted to int
+        player.positions.push(Number(toPlace))
+        //place all positions on board, check if player has won and change current player
+        render()
+        gameboard.compare(player)
+        if(gameboard.toPlay === "pvp"){
+            changePlayer()
+        }else if (gameboard.toPlay === "pvc"){
+            changePlayer()
+            compMove()
+        }
+        
+    }
+
+    const compMove = ()=>{
+        let cMove = Math.floor(Math.random() * 10);
+        while(player.positions.includes(cMove) == true){
+            cMove = Math.floor(Math.random() * 10);
+        }
+        gameboard.boardArray[cMove] = getPlayer()
+        player.positions.push(cMove)
+        render()
+        
+    }
+    return{getPlayer, place}
 
 })();
-gameboard.create()
+let play = document.querySelector("button.play")
+play.addEventListener("click", ()=>{
+    const toPick = document.querySelector("#play")
+    gameboard.toPlay = toPick.value
+    let start = document.querySelector(".start")
+    let game = document.querySelector(".game-board")
+    start.classList.add("hidden")
+    game.classList.remove("hidden")
+    gameboard.create()
+    console.log(gameboard.toPlay)
+
+})
+
+
 const player1 = Player("x", 1)
 const player2 = Player("o", 2)
 
-
-
 let player = player1
+
 
 
